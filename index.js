b167bc44165f40f330d6af15269fd2dfad74b1b1
Alize Nguyen <alizenguyen@gmail.com>
2019-03-09 02:39:27 -0600

    list working

--- index.js
+++ index.js
@@ -3,14 +3,76 @@ require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+//WHERE DATA IS STORED AT THE MOMENT ======================================
+let gbSignUp = [];
+let memberList = ''
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Cries')
+//NOTES =====================================================
+// const member = message.mentions.members.first()
+// const currentUser = message.author.username
+
+client.on('message', message => {
+
+  //WHEN ADDING YOURSELF =====================================================
+  if (message.content.startsWith('!join')) {
+    const currentUser = message.author.username
+
+    gbSignUp.push(currentUser)
+
+    for (i = 0; i < gbSignUp.length; i++) {
+      memberList += i+1 +'. ' + gbSignUp[i] + `\n`
+    }
+      
+    message.channel.send(`${currentUser} was added.`)
+
+    message.channel.send({
+      embed: {
+        color: 0x0abab5,
+        fields: [{
+            name: 'GUILD BATTLE SIGN UP',
+            value: `${memberList}`
+          },
+        ],
+      }
+    });
+
+    memberList = ''
   }
+
+  //WHEN ADDING ANOTHER MEMBER =====================================================
+  if (message.content.startsWith('!add')) {
+    const member = message.mentions.members.first()
+    
+    if (!member) {
+      return message.reply(`Who are you trying to add?.`)
+    } else {
+      gbSignUp.push(member)
+    }
+
+    for (i = 0; i < gbSignUp.length; i++) {
+      memberList += i+1 +'. ' + gbSignUp[i] + `\n`
+    }
+
+    message.channel.send(`${member} was added.`)
+
+    message.channel.send({
+      embed: {
+        color: 0x0abab5,
+        fields: [{
+            name: 'GUILD BATTLE SIGN UP',
+            value: `${memberList}`
+          },
+        ],
+      }
+    });
+
+    memberList = ''
+  }
+
 })
 
 client.login(process.env.BOT_TOKEN)
