a0adee5dcf665cacc60235d911cd566c2bec991c
Alize Nguyen <alizenguyen@gmail.com>
2019-03-11 18:30:41 -0500

    clear, remove functions added

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const client = new Discord.Client()
 
 //WHERE DATA IS STORED AT THE MOMENT ======================================
 let gbSignUp = [];
-let memberList = ''
+let gbMemberList = [];
+let count = 0;
+let memberList = '';
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
@@ -20,12 +22,11 @@ client.on('message', message => {
   //WHEN ADDING YOURSELF =====================================================
   if (message.content.startsWith('!join')) {
     const currentUser = message.author.username
-
-    gbSignUp.push(currentUser)
-
-    for (i = 0; i < gbSignUp.length; i++) {
-      memberList += i+1 +'. ' + gbSignUp[i] + `\n`
-    }
+    gbMemberList.push(currentUser);
+    
+    count = count + 1
+    
+    memberList += count +'. ' + currentUser + `\n`
       
     message.channel.send(`${currentUser} was added.`)
 
@@ -39,22 +40,20 @@ client.on('message', message => {
         ],
       }
     });
-
-    memberList = ''
   }
 
   //WHEN ADDING ANOTHER MEMBER =====================================================
   if (message.content.startsWith('!add')) {
-    const member = message.mentions.members.first()
+    // const member = message.mentions.members.first()
+    const member = message.content.substring(5);
+
+    count = count + 1
     
     if (!member) {
       return message.reply(`Who are you trying to add?.`)
     } else {
-      gbSignUp.push(member)
-    }
-
-    for (i = 0; i < gbSignUp.length; i++) {
-      memberList += i+1 +'. ' + gbSignUp[i] + `\n`
+      memberList += count +'. ' + member + `\n`
+      gbMemberList.push(member);
     }
 
     message.channel.send(`${member} was added.`)
@@ -69,16 +68,64 @@ client.on('message', message => {
         ],
       }
     });
-
-    memberList = ''
   }
 
   //WHEN CLEARING LIST =====================================================
   if (message.content.startsWith('!clear')) {
     message.channel.send('Guild Battle Sign Up is Cleared.')
-    gbSignUp = []
+    count = 0;
+    memberList = ''
+    gbMemberList = [];
   }
 
+  //WHEN VIEWING MEMBER LIST =====================================================
+  if (message.content.startsWith('!view')) {
+    if (memberList === '') {
+      message.channel.send('No one signed up.')
+    } else {
+      message.channel.send({
+        embed: {
+          color: 0x0abab5,
+          fields: [{
+              name: 'GUILD BATTLE SIGN UP',
+              value: `${memberList}`
+            },
+          ],
+        }
+      });
+    };
+  }
+
+  //WHEN REMOVING A MEMBER =====================================================
+  if (message.content.startsWith('!remove')) {
+    const removedMember = message.content.substring(8);
+
+    let index = gbMemberList.indexOf(removedMember);
+
+    if (index > -1) {
+      gbMemberList.splice(index, 1);
+    }
+
+    count = 1;
+    memberList = '';
+
+    for (i = 0; i < gbMemberList.length; i++) {
+      memberList += count +'. ' + gbMemberList[i] + `\n`
+    }
+
+    message.channel.send(`${removedMember} was removed.`)
+
+    message.channel.send({
+      embed: {
+        color: 0x0abab5,
+        fields: [{
+            name: 'GUILD BATTLE SIGN UP',
+            value: `${memberList}`
+          },
+        ],
+      }
+    });
+  }
 })
 
 client.login(process.env.bot_token)
