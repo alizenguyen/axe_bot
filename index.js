12a5809247dfeff9320b4b9fdc76c6e15d00161d
Alize Nguyen <alizenguyen@gmail.com>
2019-03-11 20:27:46 -0500

    leave function added

--- index.js
+++ index.js
@@ -44,38 +44,47 @@ client.on('message', message => {
 
   //WHEN ADDING ANOTHER MEMBER =====================================================
   if (message.content.startsWith('!add')) {
-    // const member = message.mentions.members.first()
-    const member = message.content.substring(5);
+    if (message.member.permissions.has('ADMINISTRATOR')) {
+      // const member = message.mentions.members.first()
+      const member = message.content.substring(5);
 
-    count = count + 1
-    
-    if (!member) {
-      return message.reply(`Who are you trying to add?.`)
-    } else {
-      memberList += count +'. ' + member + `\n`
-      gbMemberList.push(member);
-    }
-
-    message.channel.send(`${member} was added.`)
-
-    message.channel.send({
-      embed: {
-        color: 0x0abab5,
-        fields: [{
-            name: 'GUILD BATTLE SIGN UP',
-            value: `${memberList}`
-          },
-        ],
+      count = count + 1
+      
+      if (!member) {
+        return message.reply(`Who are you trying to add?`)
+      } else {
+        memberList += count +'. ' + member + `\n`
+        gbMemberList.push(member);
       }
-    });
+
+      message.channel.send(`${member} was added.`)
+
+      message.channel.send({
+        embed: {
+          color: 0x0abab5,
+          fields: [{
+              name: 'GUILD BATTLE SIGN UP',
+              value: `${memberList}`
+            },
+          ],
+        }
+      });
+    } else {
+      message.channel.send('Sorry, only the MacDaddy can do that.')
+    }
   }
+
 
   //WHEN CLEARING LIST =====================================================
   if (message.content.startsWith('!clear')) {
-    message.channel.send('Guild Battle Sign Up is Cleared.')
-    count = 0;
-    memberList = ''
-    gbMemberList = [];
+    if (message.member.permissions.has('ADMINISTRATOR')) {
+      message.channel.send('Guild Battle Sign Up is Cleared.')
+      count = 0;
+      memberList = ''
+      gbMemberList = [];
+    } else {
+      message.channel.send('Sorry, only the MacDaddy can do that.')
+    }
   }
 
   //WHEN VIEWING MEMBER LIST =====================================================
@@ -98,12 +107,50 @@ client.on('message', message => {
 
   //WHEN REMOVING A MEMBER =====================================================
   if (message.content.startsWith('!remove')) {
-    const removedMember = message.content.substring(8).toLowerCase();
+    if (message.member.permissions.has('ADMINISTRATOR')) {
+      const removedMember = message.content.substring(8).toLowerCase();
+
+      for (i = 0; i < gbMemberList.length; i++) {
+        const char = gbMemberList[i].toLowerCase();
+
+        if (char === removedMember){
+            gbMemberList.splice(i, 1);
+        }
+      }
+
+      count = 0;
+      memberList = '';
+
+      for (i = 0; i < gbMemberList.length; i++) {
+        count = count + 1
+        memberList += count +'. ' + gbMemberList[i] + `\n`
+      }
+
+      message.channel.send(`${removedMember} was removed.`)
+
+      message.channel.send({
+        embed: {
+          color: 0x0abab5,
+          fields: [{
+              name: 'GUILD BATTLE SIGN UP',
+              value: `${memberList}`
+            },
+          ],
+        }
+      }); 
+    } else {
+      message.channel.send('Sorry, only the MacDaddy can do that.')
+    }
+  }
+
+  //WHEN LEAVING =====================================================
+  if (message.content.startsWith('!leave')) {
+    const currentUser = message.author.username.toLowerCase();
 
     for (i = 0; i < gbMemberList.length; i++) {
       const char = gbMemberList[i].toLowerCase();
 
-      if (char === removedMember){
+      if (char === currentUser){
           gbMemberList.splice(i, 1);
       }
     }
@@ -116,25 +163,29 @@ client.on('message', message => {
       memberList += count +'. ' + gbMemberList[i] + `\n`
     }
 
-    message.channel.send(`${removedMember} was removed.`)
+    message.channel.send(`${currentUser} was removed.`)
 
-    message.channel.send({
-      embed: {
-        color: 0x0abab5,
-        fields: [{
-            name: 'GUILD BATTLE SIGN UP',
-            value: `${memberList}`
-          },
-        ],
-      }
-    });
+    if (memberList.length !== 0) {
+      message.channel.send({
+        embed: {
+          color: 0x0abab5,
+          fields: [{
+              name: 'GUILD BATTLE SIGN UP',
+              value: `${memberList}`
+            },
+          ],
+        }
+      });
+    } else {
+      message.channel.send('No one is prepared for battle. Cries.')
+    }
   }
 })
 
-client.login(process.env.bot_token)
-  .catch( error => console.log(error))
-
-// client.login('')
+// client.login(process.env.bot_token)
 //   .catch( error => console.log(error))
+
+client.login('NTU0MzU1Nzc1MTY2NzQyNTMz.D2iVkQ.ptE7suiEDPaKCRUG3t070MdbkWU')
+  .catch( error => console.log(error))
 
 require('http').createServer().listen()
